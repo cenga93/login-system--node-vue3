d47fd0038fadc370aad38b7f176564597b802c4c
Milan Milic <cenga93@gmail.com>
2022-01-23 00:23:35 +0100

    Added reactive register page data...

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -6,24 +6,40 @@
                     <div class="register__content">
                          <form class="register__form" @submit.prevent="handleSubmit">
                               <div class="input input--firstname">
-                                   <input type="text" name="firstname" autocomplete="firstname" placeholder="Firstname" />
+                                   <input
+                                        type="text"
+                                        name="firstname"
+                                        v-model="data.firstname"
+                                        autocomplete="firstname"
+                                        placeholder="Firstname"
+                                   />
                               </div>
-
                               <div class="input input--lastname">
-                                   <input type="text" name="lastname" autocomplete="lastname" placeholder="Lastname" />
+                                   <input
+                                        type="text"
+                                        name="lastname"
+                                        v-model="data.lastname"
+                                        autocomplete="lastname"
+                                        placeholder="Lastname"
+                                   />
                               </div>
 
                               <div class="input input--email">
-                                   <input type="email" name="email" autocomplete="email" placeholder="Email" />
+                                   <input type="email" name="email" v-model="data.email" autocomplete="email" placeholder="Email" />
                               </div>
 
                               <div class="input input--password">
-                                   <input type="password" name="password" autocomplete="password" placeholder="Password" />
+                                   <input
+                                        type="password"
+                                        name="password"
+                                        v-model="data.password"
+                                        autocomplete="password"
+                                        placeholder="Password"
+                                   />
                               </div>
 
                               <div class="register__content__bottom">
-                                   <router-link to="/register" class="register__link">Register</router-link>
-                                   <router-link to="/register" class="register__link">Forgot your password?</router-link>
+                                   <router-link to="/login" class="register__link">Back to login</router-link>
                               </div>
                               <button class="register__submit">Sign in</button>
                          </form>
@@ -34,8 +50,22 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
      name: 'Register',
+     setup() {
+          const data = ref({
+               firstname: '',
+               lastname: '',
+               email: '',
+               password: '',
+          });
+
+          return {
+               data,
+          };
+     },
 };
 </script>
 
